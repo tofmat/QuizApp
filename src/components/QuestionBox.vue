d1c157571ced74fd2579b83ecf63833b250fd2e0
tofmat <tofmatt@gmail.com>
2020-01-18 14:36:29 +0100

    Added an api to fetch random quiz questions

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-jumbotron>
+            <template slot="lead">
+                {{ currentQuestion.question }}
+            </template>
+
+            <hr class="my-4" />
+
+            <p>
+                List of Answers
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button> 
+            <b-button variant="success" href="#">Next</b-button>
+           
+
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object
+    }
+}
+</script>
